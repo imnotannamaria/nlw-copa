d2785eae5ffd6c933da6dbf0d06bfa8695b547a6
Anna Maria <aannamariabr@outlook.com>
2022-11-04 13:00:19 -0300

    Criação da estrutura do server, web e mobile --AULA 01

---
+++ mobile/src/screens/SignIn.tsx
@@ -0,0 +1,10 @@
+import { Center, Text } from "native-base";
+import React from "react";
+
+export function SignIn() {
+  return (
+    <Center flex={1} bg="gray.900">
+      <Text color="white" fontSize={24} fontFamily="heading">OPA</Text>
+    </Center>
+  )
+}
